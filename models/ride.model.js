cfa3d976ccbf447968e5e52576bbc2a43232b39e
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-12-09 11:46:46 +0530

    models:fix derp in rideModels

--- models/ride.model.js
+++ models/ride.model.js
@@ -1,87 +1,58 @@
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
-const captainSchema = new mongoose.Schema({
-    fullname: {
-        firstname: {
-            type: String,
-            required: true,
-            minlength: [3, 'First name must be at least 3 characters long'],
-        },
-        lastname: {
-            type: String,
-            required: true,
-            minlength: [3, 'Last name must be at least 3 characters long'],
-        }
+
+const rideSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
     },
-    email: {
+    captain: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'captain',
+    },
+    pickup: {
         type: String,
         required: true,
-        unique: true,
-        lowercase: true,
-        validate: {
-            validator: function(value) {
-                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(value);
-            },
-            message: props => `${props.value} is not a valid email address`
-        }
     },
-    password: {
+    destination: {
         type: String,
         required: true,
-        minlength: [6, 'Password must be at least 6 characters long'],
     },
-    socketID: {
-        type: String,
+    fare: {
+        type: Number,
+        required: true,
     },
+
     status: {
         type: String,
-        enum: ['active', 'inactive'],
-        default: 'inactive',
+        enum: [ 'pending', 'accepted', "ongoing", 'completed', 'cancelled' ],
+        default: 'pending',
     },
-    vehicle: {
-        color:{
-            type: String,
-            required: true,
-            minlength: [3, 'Color must be at least 3 characters long'],
-        },
-        plate: {
-            type: String,
-            required: true,
-            minlength: [3, 'Plate must be at least 3 characters long'],
-        },
-        capacity: {
-            type: Number,
-            required: true,
-            min: [1, 'Capacity must be at least 1'],
-        },
-        vehicleType: {
-            type: String,
-            required: true,
-            enum: ['motorcycle', 'motorcycle', 'car'],
-        }
+
+    duration: {
+        type: Number,
+    }, // in seconds
+
+    distance: {
+        type: Number,
+    }, // in meters
+
+    paymentID: {
+        type: String,
     },
-    location:{
-        ltd: {
-            type: Number,
-        },
-        lng: {
-            type: Number,
-        }
-    }
-});
+    orderId: {
+        type: String,
+    },
+    signature: {
+        type: String,
+    },
 
-captainSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-    return token;
-}
-captainSchema.methods.comparePassword = async function (password){
-    return await bcrypt.compare(password, this.password);
-}
-captainSchema.statics.hashPassword = async function (password){
-    return await bcrypt.hash(password, 10); // 10 is the salt rounds
-}
-const captainModel = mongoose.model('captain', captainSchema);
+    otp: {
+        type: String,
+        select: false,
+        required: true,
+    },
+})
 
-module.exports =captainModel;
+module.exports = mongoose.model('ride', rideSchema);
