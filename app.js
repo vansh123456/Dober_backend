df16dc0764f5441b1b5b2f8b4536e9054de45787
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-01-29 19:51:58 +0530

    cors policy

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app =express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    credentials: true // Important if you're sending cookies or using authentication
+}));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
